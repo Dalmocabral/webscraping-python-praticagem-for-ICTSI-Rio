ad9da7188c1ff54c8d4e18073d5e4bfce6f5fe95
Dalmo dos Santos Cabral <dalmo.santos.cabral@gmail.com>
2022-02-21 06:39:44 -0300

    commit

--- praticagem.py
+++ praticagem.py
@@ -2,8 +2,8 @@ import pandas as pd
 import json
 
 def raspagem():
-    df = pd.read_html('http://www.praticagem-rj.com.br/')
-    tabela = df[14]
+    df = pd.read_html('http://www.praticagem-rj.com.br/', match='TECONT2')
+    tabela = df[3]
 
 
     filter_praticagem = ['POB', 'NAVIO', 'CALADO', 'MANOBRA', 'DE', 'PARA' ]
